233c19faa2a4ee73bda9387cd1862f87bfe586a8
SoJeonghu <thwjdgn@gmail.com>
2024-08-01 17:40:40 +0900

    jh

--- lib/view/useNotify/taxiNotifyController.dart
+++ lib/view/useNotify/taxiNotifyController.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:delivery_taxi/data/callHistroyData.dart';
-import 'package:delivery_taxi/data/getUsage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
